783a612dbc63bc937bcc95d3e3951debe8946d3c
Efren Bautista <ing.efrenbl@gmail.com>
2020-09-02 23:15:17 -0500

    commit dos

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.port || 3000;
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Wazzappp");
 });
