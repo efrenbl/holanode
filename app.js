034bcb35c544e10c287f57543ec04a837eb4e349
Efren Bautista <ing.efrenbl@gmail.com>
2020-09-02 23:18:51 -0500

    se agrega 200

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const port = process.env.port || 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-    res.send('hello world');
+    res.status(200);
+
 });
 
 app.listen(port, () => {
