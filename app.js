d7a9c3cbdeec863baf83c08ec8bed95b30322be6
Efren Bautista <ing.efrenbl@gmail.com>
2020-09-03 00:16:53 -0500

    refactoring tres

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, function() {
     console.log(new Date().toISOString() + ": server started on port 3000");
